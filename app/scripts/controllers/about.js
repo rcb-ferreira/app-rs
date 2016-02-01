5771f1f50e9b32595c9574f247448ad22db8075e
Kgaugelo Bopape <davidfaux@Nepstar-2.local>
2016-02-01 13:51:08 +0200

    latest changes

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appArsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appArsApp
+ */
+angular.module('appArsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appArsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appArsApp
+ */
+angular.module('appArsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
